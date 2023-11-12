ab2a8b44ba493a18fbf721610793b031b2e36784
sebastian medina <sebastianlopez_21@hotmail.com>
2023-11-12 18:36:30 -0500

    Challenge Resolved

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,11 +1,33 @@
 function balancingParentheses(string) {
   // type your code here
+  let countOpen = 0;
+  let countClosed = 0;
+
+  let neededP = 0;
+
+  Array.from(string).forEach((char) => char === '(' ? countOpen += 1 : countClosed +=1)
+
+  if (countOpen > countClosed){
+    neededP = countOpen - countClosed;
+  }else if(string.charAt(0) == ")" && string.charAt(string.length-1) == "("){
+    neededP = 2;
+  }else{
+    neededP = countClosed - countOpen;
+  }
+
+  return neededP;
+  
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
   console.log(balancingParentheses('(()())'));
+
+  console.log("");
+
+  console.log("Expecting: 2")
+  console.log(balancingParentheses(')('));
 
   console.log("");
 
